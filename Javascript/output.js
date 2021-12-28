e120175788bbc3e47de43cffd50e8a1fc7887ce8
KanchanSolanke <ksolanke@vibrenthealth.com>
2021-12-28 08:18:19 +0530

    calculator in js

--- Javascript/output.js
+++
@@ -1,5 +0,0 @@
-document.write("The document.write method : ",5 + 6);
-console.log("Console method",5 + 6);
-document.getElementById("demo").innerHTML = 5 + 6;
-window.alert("It shows alert",5 + 6);
-prompt("Enter a value")
